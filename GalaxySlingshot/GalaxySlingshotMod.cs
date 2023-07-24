681c3ec02c13bc9e7188c4129110a40601f92f33
Adham084 <79041023+Adham084@users.noreply.github.com>
2023-07-24 09:53:47 +0300

    initial commit

---
+++ GalaxySlingshot/GalaxySlingshotMod.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Tools;
+
+namespace GalaxySlingshot
+{
+	public class GalaxySlingshotMod : Mod
+	{
+		public override void Entry(IModHelper helper)
+		{
+			Harmony harmony = new(ModManifest.UniqueID);
+
+			harmony.Patch(
+				AccessTools.Method(typeof(Utility), nameof(Utility.getAdventureShopStock)),
+				postfix: new HarmonyMethod(typeof(GalaxySlingshotMod), nameof(GetAdventureShopStockPostfix))
+			);
+		}
+
+		public static void GetAdventureShopStockPostfix(Dictionary<ISalable, int[]> __result)
+		{
+			if (Game1.player.mailReceived.Contains("galaxySword"))
+			{
+				__result.Add(new Slingshot(34), new int[2] { 25000, int.MaxValue });
+			}
+		}
+	}
+}
